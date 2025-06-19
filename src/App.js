b1009fbaabf1589f11f66043677f1b47b70bda7b
Tarun Thakur <tarunthakur285@gmail.com>
2025-06-19 15:10:20 +0530

    Add user dashboard component and update routing in App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Login from "./login";
 import Applayout from "./layout/Applayout";
 
 function App() {
+  const[userDetails, setUserDetails] = useState(null);
+  const updateuserDetails=(updatedUserDetails)=>{
+    setUserDetails(updatedUserDetails);
+  };
   return (
     <Routes>
       <Route path="/" element={<Applayout><Home /></Applayout>} />
       <Route path="/login" element={<Applayout><Login /></Applayout>} />
+      <Route path="/dashboard" element={<dashboard />} />
     </Routes>
   );
 }
---
+++ src/pages/dashboard.js
@@ -0,0 +1,8 @@
+function dashboard() {
+    return(
+        <div className="container text-center">
+            <h1>User Dashboard Page</h1>
+        </div>
+    );
+}
+export default dashboard;
